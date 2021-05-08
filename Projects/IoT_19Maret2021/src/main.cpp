5b5a9c26511a84e26e4fcc04b5e7f3b5b1317b00
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-05-08 15:40:14 +0800

    konek wifi gratis iconplus

--- Projects/IoT_19Maret2021/src/main.cpp
+++ Projects/IoT_19Maret2021/src/main.cpp
@@ -1,31 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-const char* WIFI_SSID = "ArduinoAP";
-const char* WIFI_PASS = "Lontongsayur";
-const char* HOSTNAME = "BoardESP32IOTSore";
-
+const char* WIFI_SSID = "WIFI gratis By ICON+";
+const char* WIFI_PASS = "1sampai8";
+const char* HOSTNAME = "BoardESP32";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-
-  WIFI.mode(WIFI_STA);
-  WIFI.begin(WIFI_SSID, WIFI_PASS);
-  WIFI.setHostname(HOSTNAME);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  WiFi.setHostname(HOSTNAME);
 
   Serial.print("Connecting to WiFi");
-  while(Wifi.status() !- WL_CONNECTED)
+  while(WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(1000);
   }
   Serial.println("");
-  Serial.println("WiFi Connected Succsessfully")
+  Serial.println("WiFi Connected Succsessfully");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Guntur Nanggala Sakti");
-  delay(1000);
+  Serial.begin(115200);
+
 }
